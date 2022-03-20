fcd8c9e461fcbffd8fa2f34a98775192fadfd4fe
azrilalkahfi <99373235+azrilalkahfi@users.noreply.github.com>
2022-03-20 11:43:48 +0700

    Add files via upload

---
+++ terkutuk.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+clear
+sleep 1
+toilet -f big " ALL VIRUS " -F gay
+echo
+sleep 1
+echo -e "-----------------------------------------------------------------"
+echo -e " AUTHOR -> AZRIL 👩‍💻 "
+echo -e " TEAM => 7 Dark Club "
+echo -e " Github => https://chat.whatsapp.com/GfDPRMb91AD8UXpD2jbJVD "
+echo -e "-----------------------------------------------------------------"
+echo
+echo -e "1.] SEND MALWARE 🎭"
+echo -e "2.] SEND VIRUS 🎭"
+echo -e "3.] ( DAN ADA BONUS ) > SEND TERKUTUK 🎭"
+echo
+read -p "pilih => " virus
+if [[ $virus == "1" ]]
+then
+read -p "masukkan nomor target => : " nomor
+while [[ true ]]; do
+echo " SEND MALWARE 🎭 KE $nomor telah berhasil "
+done
+fi
+
+if [[ $virus == "2" ]]
+then
+read -p "masukkan nomor target => : " nomor2
+while [[ true ]]; do
+echo " SEND VIRUS 🎭 KE $nomor2 telah berhasil "
+done
+fi
+
+if [[ $virus == "3" ]]
+then
+read -p "masukkan nomor target => : " nomor3
+while [[ true ]]; do
+echo " SEND TERKUTUK 🎭 KE $nomor3 telah berhasil "
+done
+fi
